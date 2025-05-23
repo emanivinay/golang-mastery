486acd2e9d0f17ff9c8f8968d8ca8fc409245696
Vinay Emani <emanivinay@outlook.com>
2025-05-23 11:09:21 +0530

    Basic error handling in greetings.Hello

--- cmd/greeter/main.go
+++ cmd/greeter/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/emanivinay/golang-mastery/greetings"
 )
 
+const (
+	ERR_NO_CLI_ARGS          = 1
+	ERR_EMPTY_CLI_ARG_STRING = 2
+)
+
 func main() {
-	greeting := greetings.Hello("Vinay")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("must provide atleast one non-empty cli argument"))
+		os.Exit(ERR_NO_CLI_ARGS)
+	}
+	greeting, err := greetings.Hello(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(ERR_EMPTY_CLI_ARG_STRING)
+	}
 	fmt.Println(greeting)
 }
